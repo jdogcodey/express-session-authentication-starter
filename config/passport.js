6ead071c9e4eb269cd35de5170f98a3e1fd1dc96
jdogcodey <jonnydawson@hotmail.com>
2025-01-15 19:36:36 +0000

    Followed tutorial, successfully switched it to psql from mongodb and fixed errors

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,53 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
-const User = connection.models.User;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+// const connection = require('./database');
+// const User = connection.models.User;
+const validPassword = require("../lib/passwordUtils").validPassword;
 
-// TODO: passport.use();
+const pool = require("./pool");
+
+const customFields = {
+  usernameField: "uname",
+  passwordField: "pw",
+};
+
+const verifyCallback = async (username, password, done) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    const user = rows[0];
+
+    if (!user) {
+      return done(null, false, { message: "Incorrect username" });
+    }
+    const match = validPassword(password, user.hash, user.salt);
+    if (!match) {
+      return done(null, false, { message: "Incorrect password" });
+    }
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+};
+
+const strategy = new LocalStrategy(customFields, verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
